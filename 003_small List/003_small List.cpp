5b97acb4aef9dad184bf6892ec8b63e3e01d051f
SOOHYUK012 <shhappy3765@naver.com>
2024-09-04 14:33:51 +0900

    Add files via upload

---
+++ 003_small List/003_small List.cpp
@@ -0,0 +1,18 @@
+﻿// 003_small List.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    float a = 9.8;
+    float b = 3.14;
+    float sum = a + b;
+    float sub = a - b;
+    float mul = a * b;
+    float div = a / b;
+    //float mod = a%b;
+
+    
+}
+
---
+++ 009_size of/009_size of.cpp
@@ -0,0 +1,25 @@
+﻿// 009_size of.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("sizeof char= %d\n", sizeof(char));
+    printf("sizeof short= %d\n", sizeof(short));
+    printf("sizeof long= %d\n", sizeof(long));
+    printf("sizeof int= %d\n", sizeof(unsigned int));
+    printf("sizeof longlong= %d\n", sizeof(long long));
+
+    printf("sizeof float= %d\n", sizeof(float));
+    printf("sizeof double= %d\n", sizeof(double));
+
+    char c;
+    short s;
+    long 1;
+    int i;
+    long long 11;
+    float f;
+    double d;
+}
+
