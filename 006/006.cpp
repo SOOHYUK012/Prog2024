bd26c7134945956328c2e0bd6f1d15c441740c8b
SOOHYUK012 <shhappy3765@naver.com>
2024-09-07 23:58:53 +0900

    Add files via upload

---
+++ 006/006.cpp
@@ -0,0 +1,18 @@
+﻿// 006.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("화씨           섭씨"\n);
+    printf("=================="\n);
+        
+    float a = 100.0;
+    float b = 80.0;
+    float c = 60.0;
+    
+    printf("")
+     
+}
+
---
+++ 007_BMI/007_BMI.cpp
@@ -0,0 +1,22 @@
+﻿// 007_BMI.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    float weight, height;
+
+    printf("몸무게 : ");
+    scanf_s("%f", &weight);
+    printf("키(CM) : ");
+    scanf_s("%f", &height);
+
+    height /= 100;  // m 단위로 변환
+
+    float bmi = (weight / (height * height));
+
+    printf("bmi : %.1f", bmi);
+    
+}
+
